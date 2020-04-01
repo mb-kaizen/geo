0166e515d2c449c3a5d921b6caf9a7031e39e55e
MB <michael.entr@gmail.com>
2020-04-01 17:18:26 +0900

    reconfigured asset preprocessing for heroku

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module Geo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
-    
+    # config.assets.initialize_on_precompile = false
+
 
     config.assets.paths << Rails.root.join("data")
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.precompile = ['*.js', '*.css']
+  config.assets.precompile = ['*.js', '*.scss']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
